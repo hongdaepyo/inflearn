4146aa074f27890598867af679d94ec5dbaca9b8
Hong Daepyo <h1o1n1g1@nate.com>
2021-05-22 02:11:36 +0900

    feat(tdd) 더 자바, 애플리케이션을 테스트하는 다양한 방법 첫번째 커밋

---
+++ inflearn-the-java-test/src/test/java/com/dphong/inflearnthejavatest/InflearnTheJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dphong.inflearnthejavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnTheJavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
