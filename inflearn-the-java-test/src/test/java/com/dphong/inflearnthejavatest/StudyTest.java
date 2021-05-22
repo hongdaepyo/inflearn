2b416acc22ac7d38b59bd4187b6ddd43df9306d5
Hong Daepyo <h1o1n1g1@nate.com>
2021-05-23 02:29:28 +0900

    feat(tdd): JUnit 5 태깅과 필터링

--- inflearn-the-java-test/src/test/java/com/dphong/inflearnthejavatest/StudyTest.java
+++ inflearn-the-java-test/src/test/java/com/dphong/inflearnthejavatest/StudyTest.java
@@ -1,25 +1,35 @@
 package com.dphong.inflearnthejavatest;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.DisabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
     @DisplayName("스터디 만들기")
+//    @EnabledOnOs(OS.LINUX)
+    @DisabledOnJre(JRE.OTHER)
     void create_new_study() {
-//        Study study = new Study(-10);
-//        assertNotNull(study);
-//        assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들면 상태값이 DRAFT여야 한다.");
-//        assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0 보다 커야한다.");
+        String test_env = System.getenv("TEST_ENV");
 
+        System.out.println(test_env);
+//        assumeTrue("LOCAL".equalsIgnoreCase(test_env));
+//        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
+//
+//        });
+//
+//        assumingThat("dphong".equalsIgnoreCase(test_env), () -> {
+//
+//        });
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
 
         assertEquals("limit은 0보다 커야 한다.", exception.getMessage());
