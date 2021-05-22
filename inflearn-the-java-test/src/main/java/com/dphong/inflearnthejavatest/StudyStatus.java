22d87bdf1dcebb49dec45ab941d6e93e3e221e16
Hong Daepyo <h1o1n1g1@nate.com>
2021-05-23 02:10:56 +0900

    feat(tdd): JUnit5 Assertion

---
+++ inflearn-the-java-test/src/main/java/com/dphong/inflearnthejavatest/StudyStatus.java
@@ -0,0 +1,5 @@
+package com.dphong.inflearnthejavatest;
+
+public enum StudyStatus {
+    DRAFT, STARTED, ENDED
+}
--- inflearn-the-java-test/src/test/java/com/dphong/inflearnthejavatest/StudyTest.java
+++ inflearn-the-java-test/src/test/java/com/dphong/inflearnthejavatest/StudyTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -13,13 +15,24 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기")
     void create_new_study() {
-        Study study = new Study();
-        assertNotNull(study);
+//        Study study = new Study(-10);
+//        assertNotNull(study);
+//        assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들면 상태값이 DRAFT여야 한다.");
+//        assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0 보다 커야한다.");
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+
+        assertEquals("limit은 0보다 커야 한다.", exception.getMessage());
+
+//        assertTimeout(Duration.ofMillis(100), () -> {
+//            new Study(10);
+//            Thread.sleep(300);
+//        });
     }
 
     @Test
     void create_new_study_again() {
-        Study study = new Study();
-        assertNotNull(study);
+//        Study study = new Study();
+//        assertNotNull(study);
     }
 }
