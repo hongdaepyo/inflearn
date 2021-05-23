83a944e81df703c7aa2a3d8f125af64b8eb6762f
Hong Daepyo <h1o1n1g1@nate.com>
2021-05-23 12:52:22 +0900

    feat(tdd): JUnit 5 테스트 반복하기 1부

--- inflearn-the-java-test/src/test/java/com/dphong/inflearnthejavatest/StudyTest.java
+++ inflearn-the-java-test/src/test/java/com/dphong/inflearnthejavatest/StudyTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.condition.DisabledOnJre;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.JRE;
 import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
 
@@ -45,5 +47,18 @@ class StudyTest {
 //        Study study = new Study();
 //        assertNotNull(study);
         System.out.println("slow");
+    }
+
+    @DisplayName("스터디 만들기 반복")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    void repeatTest(RepetitionInfo repetitionInfo) {
+        System.out.println("test" + repetitionInfo.getCurrentRepetition() + "/" +
+                repetitionInfo.getTotalRepetitions());
+    }
+
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요"})
+    void parameterizedTest(String message) {
+        System.out.println(message);
     }
 }
